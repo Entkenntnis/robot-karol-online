aa28cd42ebeda3fb8f3a10c0793dd38c554d3e6a
Entkenntnis <Entkenntnis@users.noreply.github.com>
2022-05-29 18:26:13 +0200

    add p5, downgrade to fix regression

--- lib/data/puzzles.tsx
+++ lib/data/puzzles.tsx
@@ -3,64 +3,14 @@ import { p1_start } from './p1_start'
 import { p2_smileys } from './p2_smileys'
 import { p3_fliesen } from './p3_fliesen'
 import { p4_säulen } from './p4_säulen'
+import { p5_lol } from './p5_lol'
 
 export const puzzles: Puzzle[] = [
   p1_start,
   p2_smileys,
   p3_fliesen,
   p4_säulen,
-
-  {
-    id: 5,
-    title: 'TODO 5',
-    posX: 19,
-    posY: 15,
-    deps: [1],
-    description: (
-      <>
-        <p className="mb-2">
-          Ein schöner Fußboden macht immer eine Freude. Lege bei dieser Aufgabe
-          den Boden einmal komplett mit Fliesen aus:
-        </p>
-        <img
-          src="/puzzle/fliesen.png"
-          alt="target"
-          className="mx-auto my-3 max-h-[120px]"
-        ></img>
-        <p className="mb-2">
-          Bei diesem Programm kommt eine Schleife zum Einsatz: Mit dem
-          Schlüsselwort <em>wiederhole solange</em> kann Karol eine Aktion
-          solange wiederholen, bis die Bedingung nicht mehr erfüllt ist. In
-          diesem Fall legt er Fliesen bis zur nächsten Wand.
-        </p>
-      </>
-    ),
-    targetWorld: {
-      dimX: 10,
-      dimY: 4,
-      height: 6,
-      karol: { x: 0, y: 3, dir: 'west' },
-      bricks: [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      ],
-      marks: [
-        [true, true, true, true, true, true, true, true, true, true],
-        [true, true, true, true, true, true, true, true, true, true],
-        [true, true, true, true, true, true, true, true, true, true],
-        [true, true, true, true, true, true, true, true, true, true],
-      ],
-      blocks: [
-        [false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false],
-      ],
-    },
-    code: 'wiederhole solange NichtIstWand\n  MarkeSetzen\n  Schritt\nendewiederhole\nMarkeSetzen',
-  },
+  p5_lol,
 
   {
     id: 6,
