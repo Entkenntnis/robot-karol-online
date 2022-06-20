299350f21c725be010b5e711c7c4596fa35504b7
Entkenntnis <Entkenntnis@users.noreply.github.com>
2022-06-20 14:55:51 +0200

    add info link to readme

--- components/Workspace.tsx
+++ components/Workspace.tsx
@@ -1,9 +1,11 @@
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 import clsx from 'clsx'
 import { ReflexElement, ReflexSplitter, ReflexContainer } from 'react-reflex'
 
 import { openMenu } from '../lib/commands/menu'
 import { useCore } from '../lib/state/core'
 import { EditArea } from './EditArea'
+import { FaIcon } from './FaIcon'
 import { FileInput } from './FileInput'
 import { Ping } from './Ping'
 import { Player } from './Player'
@@ -18,18 +20,29 @@ export function Workspace() {
       )}
       <FileInput />
       {core.ws.type == 'free' && (
-        <button
-          className={clsx(
-            'absolute right-1 top-1 rounded z-10',
-            'px-2 py-0.5 bg-blue-300 hover:bg-blue-400'
-          )}
-          onClick={() => {
-            openMenu(core)
-          }}
-        >
-          Menü
-          {core.state.inviteMenu && <Ping />}
-        </button>
+        <div className="absolute right-1 top-1 z-10">
+          <a
+            href="https://github.com/Entkenntnis/robot-karol-web#readme"
+            target="_blank"
+            className="mr-3 text-lg text-blue-500 hover:text-blue-600"
+            rel="noreferrer"
+          >
+            <FaIcon icon={faCircleInfo} style={{ verticalAlign: '-4px' }} />
+          </a>
+
+          <button
+            className={clsx(
+              ' rounded ',
+              'px-2 py-0.5 bg-blue-300 hover:bg-blue-400'
+            )}
+            onClick={() => {
+              openMenu(core)
+            }}
+          >
+            Menü
+            {core.state.inviteMenu && <Ping />}
+          </button>
+        </div>
       )}
       {core.ws.type == 'puzzle' &&
         !core.ws.preMode &&
