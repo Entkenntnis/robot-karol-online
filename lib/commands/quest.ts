62151614ac1c41d49ed8ddd97bbed4ad12d7d7c3
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-01-12 23:00:23 +0100

    fix bug

--- lib/commands/quest.ts
+++ lib/commands/quest.ts
@@ -14,6 +14,7 @@ export function runTask(core: Core, index: number) {
     ws.world = task.start
     ws.ui.showOutput = true
     ws.quest.lastStartedTask = index
+    ws.quest.progress = false
   })
   if (core.ws.ui.state == 'ready') {
     if (core.view?.current) {
