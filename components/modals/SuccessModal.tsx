d8230a50f8b57a2dd98ff6e33823f4fcba493fd9
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-06-09 14:40:02 +0200

    add user feedback option

--- components/modals/SuccessModal.tsx
+++ components/modals/SuccessModal.tsx
@@ -9,6 +9,8 @@ import { useCore } from '../../lib/state/core'
 import { FaIcon } from '../helper/FaIcon'
 import confetti from 'canvas-confetti'
 import { positiveText } from '../../lib/helper/positiveText'
+import { Rating } from 'react-simple-star-rating'
+import { submit_event } from '../../lib/helper/submit'
 
 var count = 200
 var defaults = {
@@ -53,6 +55,8 @@ export function SuccessModal() {
   const core = useCore()
   const [positive] = useState(positiveText())
 
+  const [hasFeedback, setHasFeedback] = useState(false)
+
   // closeModal(core)
   // switchToPage(core, 'overview')
 
@@ -65,12 +69,28 @@ export function SuccessModal() {
       <div className="bg-black/20 fixed inset-0 z-[150]"></div>
       <div className="fixed inset-0 flex justify-center items-center z-[200]">
         <div
-          className="h-[280px] w-[500px] bg-white z-[200] rounded-xl relative flex items-center justify-between flex-col"
+          className="w-[500px] bg-white z-[200] rounded-xl relative flex items-center justify-between flex-col"
           onClick={(e) => {
             e.stopPropagation()
           }}
         >
-          <h1 className="mt-12 text-4xl">{positive}</h1>
+          <h1 className="mt-10 text-4xl mb-8">{positive}</h1>
+          <div className="mb-2">Wie gut hat dir die Aufgabe gefallen?</div>
+          <Rating
+            className="[&_svg]:inline"
+            readonly={hasFeedback}
+            onClick={(rate) => {
+              submit_event(`rate_quest_${core.ws.quest.id}_${rate}`, core)
+              setHasFeedback(true)
+            }}
+          />
+          <div className="h-16 mt-2">
+            {hasFeedback ? (
+              <small>Danke für dein Feedback &#10084;</small>
+            ) : (
+              <small>&nbsp;</small>
+            )}
+          </div>
           <button
             onClick={() => {
               finishQuest(core)
@@ -78,7 +98,7 @@ export function SuccessModal() {
             }}
             className={clsx(
               'px-4 py-2 rounded hover:bg-green-300',
-              'bg-green-200 mx-auto mb-16 text-lg'
+              'bg-green-200 mx-auto mb-12 text-lg'
             )}
           >
             weiter
