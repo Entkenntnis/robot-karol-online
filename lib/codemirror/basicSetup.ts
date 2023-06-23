62e0af26db21fe0d4c7a5fb863e92c629063d835
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-06-23 10:58:53 +0200

    follow official docs

--- lib/codemirror/basicSetup.ts
+++ lib/codemirror/basicSetup.ts
@@ -104,16 +104,12 @@ const Theme = EditorView.theme({
     height: '100%',
   },
   '.cm-gutters': {
-    minHeight: '300px',
     minWidth: '30px',
     display: 'flex',
     justifyContent: 'end',
   },
-  '.cm-content': {
-    height: '100%',
-  },
   '.cm-scroller': {
-    overflowX: 'initial !important',
+    overflow: 'auto',
     fontFamily: 'Hack, monospace',
   },
   '.cm-completionLabel': {
