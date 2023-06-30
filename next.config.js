ba12ec8b598178d60d0a8bd69ee77e0de76eac78
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-06-30 15:44:56 +0200

    use new output config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({
+  output: 'export',
+})
