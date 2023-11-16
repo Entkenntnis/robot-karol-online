8b1835615ec20804dbd423c952a85c31b8495f23
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-11-16 13:11:48 +0100

    finishing touches snake

--- app/snake/page.tsx
+++ app/snake/page.tsx
@@ -4,11 +4,37 @@ import { useEffect, useRef } from 'react'
 import { View } from '../../components/helper/View'
 import { createWorldCmd, forward, left, right } from '../../lib/commands/world'
 import { useCreateCore } from '../../lib/state/core'
+import clsx from 'clsx'
+
+const stepDelays = [
+  // Geschwindigkeit ab LEN
+  { len: 0, ms: 600 },
+  { len: 1, ms: 400 },
+  { len: 3, ms: 300 },
+  { len: 5, ms: 200 },
+  { len: 10, ms: 150 },
+  { len: 18, ms: 120 },
+  { len: 28, ms: 100 },
+  { len: 40, ms: 80 },
+  { len: Infinity, ms: 50 },
+]
+
+function getDelay(n: number) {
+  for (let i = 0; i < stepDelays.length - 1; i++) {
+    if (n >= stepDelays[i].len && n < stepDelays[i + 1].len) {
+      // This logic is broken
+      return stepDelays[i].ms
+    }
+  }
+  return 600
+}
 
 export default function Snake() {
   const core = useCreateCore()
   const next = useRef<null | { x: number; y: number }>(null)
   const tail = useRef<{ x: number; y: number }[]>([])
+  const stepMs = useRef(600)
+  const lastStepTimestamp = useRef(-1)
 
   function spawnTarget() {
     let count = 0
@@ -39,67 +65,63 @@ export default function Snake() {
         if (!next.current) {
           spawnTarget()
         }
-        if (core.ws.code == 'ArrowLeft') {
-          left(core)
-        }
-        if (core.ws.code == 'ArrowRight') {
-          right(core)
-        }
-        core.mutateWs((state) => {
-          state.code = ''
-        })
-        const prevX = core.ws.world.karol.x
-        const prevY = core.ws.world.karol.y
-        if (!forward(core)) {
-          core.mutateWs(({ ui }) => {
-            ui.state = 'error'
-          })
-        } else {
-          tail.current.push({ x: prevX, y: prevY })
+        const ts = new Date().getTime()
+        const diff = ts - lastStepTimestamp.current
+        if (diff > stepMs.current) {
+          lastStepTimestamp.current = ts
+          // step start
+          if (core.ws.code == 'ArrowLeft') {
+            left(core)
+          }
+          if (core.ws.code == 'ArrowRight') {
+            right(core)
+          }
           core.mutateWs((state) => {
-            state.world.bricks[prevY][prevX] = 2
+            state.code = ''
           })
+          const prevX = core.ws.world.karol.x
+          const prevY = core.ws.world.karol.y
+          if (!forward(core)) {
+            core.mutateWs(({ ui }) => {
+              ui.state = 'error'
+            })
+          } else {
+            tail.current.push({ x: prevX, y: prevY })
+            core.mutateWs((state) => {
+              state.world.bricks[prevY][prevX] = 2
+            })
 
-          let noDelete = false
-          if (next.current) {
-            if (
-              core.ws.world.karol.x == next.current.x &&
-              core.ws.world.karol.y == next.current.y
-            ) {
+            let noDelete = false
+            if (next.current) {
+              if (
+                core.ws.world.karol.x == next.current.x &&
+                core.ws.world.karol.y == next.current.y
+              ) {
+                core.mutateWs((state) => {
+                  state.world.marks[core.ws.world.karol.y][
+                    core.ws.world.karol.x
+                  ] = false
+                })
+                next.current = null
+                noDelete = true
+                stepMs.current = getDelay(tail.current.length)
+              }
+            }
+            if (!noDelete) {
+              const toDelete = tail.current.shift()!
               core.mutateWs((state) => {
-                state.world.marks[core.ws.world.karol.y][
-                  core.ws.world.karol.x
-                ] = false
+                state.world.bricks[toDelete.y][toDelete.x] = 0
               })
-              next.current = null
-              noDelete = true
             }
           }
-          if (!noDelete) {
-            const toDelete = tail.current.shift()!
-            core.mutateWs((state) => {
-              state.world.bricks[toDelete.y][toDelete.x] = 0
-            })
-          }
+          // step end
         }
       }
-    }, 600)
+    }, 0)
     window.addEventListener('keydown', (e) => {
       core.mutateWs((state) => {
         state.code = e.key
       })
-      if (e.key == ' ' && core.ws.ui.state == 'loading') {
-        core.mutateWs((state) => {
-          state.ui.state = 'running'
-        })
-      }
-      if (e.key == ' ' && core.ws.ui.state == 'error') {
-        // reset game
-        createWorldCmd(core, 20, 15, 6)
-        core.mutateWs((state) => {
-          state.ui.state = 'running'
-        })
-      }
     })
     return () => {
       clearInterval(interval)
@@ -110,10 +132,10 @@ export default function Snake() {
     <div className="mt-8">
       <h1 className="text-center text-3xl">Snake Karol Edition</h1>
       <p className="text-center mt-4">
-        Steuere Karol mit der Links-/Rechtstaste. Starte das Spiel mit
-        Leertaste.
+        Steuere Karol mit den Pfeiltasten links/rechts.
       </p>
-      <div className="h-full w-full flex justify-center items-center mt-8">
+      <p className="text-center mt-4">Länge: {tail.current.length}</p>
+      <div className="h-full w-full flex justify-center items-center">
         <div className="relative">
           <View appearance={core.ws.appearance} world={core.ws.world} />
           {core.ws.ui.state == 'error' && (
@@ -122,6 +144,71 @@ export default function Snake() {
             </div>
           )}
         </div>
+      </div>
+      <div className="flex justify-center mt-6">
+        {core.ws.ui.state === 'loading' && (
+          <button
+            className="px-3 py-2 bg-green-200 hover:bg-green-300 rounded-xl"
+            onClick={() => {
+              core.mutateWs((state) => {
+                state.ui.state = 'running'
+              })
+            }}
+          >
+            Start
+          </button>
+        )}
+        {core.ws.ui.state === 'running' && (
+          <>
+            <button
+              className={clsx(
+                'px-3 py-2 bg-gray-100 hover:bg-gray-200 rounded-xl mr-6',
+                core.ws.code == 'ArrowLeft' && 'bg-gray-300'
+              )}
+              onClick={() => {
+                core.mutateWs((state) => {
+                  state.code = 'ArrowLeft'
+                })
+              }}
+            >
+              LinksDrehen
+            </button>
+            <button
+              className={clsx(
+                'px-3 py-2 bg-gray-100 hover:bg-gray-200 rounded-xl',
+                core.ws.code == 'ArrowRight' && 'bg-gray-300'
+              )}
+              onClick={() => {
+                core.mutateWs((state) => {
+                  state.code = 'ArrowRight'
+                })
+              }}
+            >
+              RechtsDrehen
+            </button>
+          </>
+        )}
+        {core.ws.ui.state === 'error' && (
+          <button
+            className="px-3 py-2 underline"
+            onClick={() => {
+              // reset game
+              createWorldCmd(core, 15, 12, 6)
+              stepMs.current = 600
+              tail.current = []
+              next.current = null
+              core.mutateWs((state) => {
+                state.ui.state = 'running'
+              })
+              core.mutateWs((state) => {
+                state.world.karol.dir = 'north'
+                state.world.karol.y = 11
+              })
+            }}
+          >
+            neu starten
+          </button>
+        )}
       </div>
     </div>
   )
