85f203a79705e433026af32e527de7b7d0645bc2
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-11-21 10:08:42 +0100

    update cutoff

--- lib/commands/init.ts
+++ lib/commands/init.ts
@@ -32,7 +32,7 @@ export async function initClient(core: Core) {
 
   if (hash == '#ANALYZE' /* && window.location.hostname == 'localhost'*/) {
     try {
-      const cutoff = new Date('2023-11-20')
+      const cutoff = new Date('2023-11-22')
 
       const storedPW = sessionStorage.getItem('karol_stored_pw')
       const password = storedPW ?? prompt('Zugangspasswort:') ?? ''
