1812631d4d300b759f5c042d54b4910211105c71
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-01-03 11:06:12 +0100

    workaround in config

--- next.config.js
+++ next.config.js
@@ -8,4 +8,9 @@ module.exports = withBundleAnalyzer({
     config.externals = [...config.externals, { canvas: 'canvas', ws: 'ws' }] // required to make blockly work
     return config
   },
+  experimental: {
+    // Workaroud for issues in deployment on uberspace
+    workerThreads: false,
+    cpus: 1,
+  },
 })
