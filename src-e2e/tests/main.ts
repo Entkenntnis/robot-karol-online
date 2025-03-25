145ddbc5d8c6ae4c9393214a54cef0c1c826e618
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-03-25 12:40:37 +0100

    expand tests

--- src-e2e/tests/main.ts
+++ src-e2e/tests/main.ts
@@ -6,7 +6,7 @@ Scenario('Is landing page loading?', ({ I }) => {
   I.seeTitleEquals('Robot Karol Online')
 })
 
-Scenario('Learning path Start', ({ I }) => {
+Scenario("Let's solve the first quests", ({ I }) => {
   I.amOnPage('/')
   I.click('Start')
   I.type('anna')
@@ -23,10 +23,69 @@ Scenario('Learning path Start', ({ I }) => {
   I.click('Start')
   I.waitForText('weiter', 10)
   I.click('weiter')
-})
-
-Scenario('Learning path Continue', ({ I }) => {
-  I.amOnPage('/')
   I.see('Verschieben')
   I.see('Um die Ecke')
+  I.see('Umweltschutz')
+
+  I.click('Umweltschutz')
+  I.wait(1)
+  I.click('div .fixed')
+  I.click('div .cm-activeLine')
+  I.type('Aufheben Schritt(2) Aufheben')
+  I.click('Start')
+  I.waitForText('weiter', 10)
+  I.click('weiter')
+
+  I.click('Um die Ecke')
+  I.wait(1)
+  I.click('div .fixed')
+  I.click('div .cm-activeLine')
+  I.type('Schritt(2) LinksDrehen Schritt Hinlegen')
+  I.click('Start')
+  I.waitForText('weiter', 10)
+  I.click('weiter')
+
+  I.click('Verschieben')
+  I.wait(1)
+  I.click('div .fixed')
+  I.click('div .cm-activeLine')
+  I.type('RechtsDrehen Aufheben LinksDrehen Schritt(3) Hinlegen')
+  I.click('Start')
+  I.waitForText('weiter', 10)
+  I.click('weiter')
+
+  I.wait(1)
+
+  I.click('Fortschritt speichern')
+  I.click('OK')
+  I.refreshPage()
+  I.dontSee('Fortschritt speichern')
+  I.see('Spiegelei')
+})
+
+Scenario.only('See if playground works fine', ({ I }) => {
+  I.amOnPage('/')
+  I.click('Spielwiese')
+  I.seeTitleEquals('Spielwiese')
+  // browser back button
+  I.usePlaywrightTo('go back', async ({ page }) => {
+    await page.goBack()
+  })
+  I.seeTitleEquals('Robot Karol Online')
+
+  I.click('Spielwiese')
+  I.seeTitleEquals('Spielwiese')
+  I.click('zurück zu Robot Karol Online')
+  I.seeTitleEquals('Robot Karol Online')
+
+  I.click('Spielwiese')
+  I.see('Programmiere frei und baue dein Herzensprojekt.')
+
+  // HOW TO DO THAT?
+  /*I.selectOption('select.rounded-lg', 'python-pro')
+  I.waitForText('Spickzettel')
+  I.wait(10)
+  I.click('Start')
+
+  I.wait(10)*/
 })
