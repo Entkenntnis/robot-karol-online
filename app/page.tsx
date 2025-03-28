1739bafc15f2bc1ca9becb7dc384ab9be77643ae
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-03-29 00:57:32 +0100

    more decent spinner design

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import { CoreProvider, useCreateCore } from '../lib/state/core'
 const App = dynamic(() => import('../components/App').then((mod) => mod.App), {
   ssr: false,
   loading: () => (
-    <div className="flex justify-center items-center h-screen bg-[#2e5691]/5">
-      <div className="w-10 h-10 border-4 border-[#ffd266] border-t-[#e96b6d] rounded-full animate-spin" />
+    <div className="absolute top-0 right-0 p-2">
+      <div className="w-6 h-6 border-2 border-[#ffd266] border-t-[#e96b6d] rounded-full animate-spin" />
     </div>
   ),
 })
