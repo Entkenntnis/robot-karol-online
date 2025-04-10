fa96380246e2e2e85d35f3532ca4a06abac4a2d5
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-04-10 10:15:53 +0200

    disable source maps

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ console.log('  > Warning about headers is expected.')
 
 module.exports = withBundleAnalyzer({
   output: 'export',
-  productionBrowserSourceMaps: true,
   // necessary for pyodide to work in a webworker, for dev
   // set proper headers in production
   async headers() {
