a0c16c0fffdb08ad4fd9d52a98f6df641e9060d5
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-04-13 18:42:58 +0200

    show structogram button if can switch

--- components/ide/Tasks.tsx
+++ components/ide/Tasks.tsx
@@ -386,8 +386,7 @@ export function Tasks() {
         </div>
         <div className={clsx('flex-grow-0 flex-shrink-0')}>
           {(core.ws.page == 'editor' ||
-            (!core.ws.ui.proMode &&
-              core.ws.settings.language != 'python-pro')) && (
+            (!core.ws.ui.proMode && core.ws.ui.pythonProCanSwitch)) && (
             <button
               className="mx-2 py-0.5 bg-gray-200 hover:bg-gray-300 px-2 rounded"
               onClick={() => {
