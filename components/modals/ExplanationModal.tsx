1fd76c0281aafb624d9940440791928020374be2
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-05-02 12:12:19 +0200

    add more immersion to the story

--- components/modals/ExplanationModal.tsx
+++ components/modals/ExplanationModal.tsx
@@ -1,4 +1,4 @@
-import { faTimes, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { closeModal } from '../../lib/commands/modal'
 import { useCore } from '../../lib/state/core'
 import { FaIcon } from '../helper/FaIcon'
@@ -6,22 +6,9 @@ import { chapterData } from '../../lib/data/chapters'
 import { processMarkdown } from '../../lib/helper/processMiniMarkdown'
 import { setQuestData } from '../../lib/storage/storage'
 import { submitAnalyzeEvent } from '../../lib/commands/analyze'
-import { mapData } from '../../lib/data/map'
-import { isQuestDone } from '../../lib/helper/session'
 
 export function ExplanationModal() {
   const core = useCore()
-  const questsInPreviousChapter = Object.entries(mapData)
-    .filter(([_, data]) => data.chapter === core.ws.overview.explanationId - 1)
-    .map(([id]) => parseInt(id))
-
-  const canContinue =
-    questsInPreviousChapter.length == 0 ||
-    questsInPreviousChapter.some((id) => {
-      return isQuestDone(id)
-    }) ||
-    core.ws.page == 'demo'
-
   return (
     <div className="fixed inset-0 z-[1350]">
       {/* Blurred background image */}
@@ -48,51 +35,33 @@ export function ExplanationModal() {
         <div className="h-screen max-w-[620px] bg-white/90 backdrop-blur-lg relative flex flex-col">
           <div className="flex-grow overflow-auto">
             <div className="px-6 mt-12 mb-8 animate-fadeInUp">
-              {canContinue ? (
-                processMarkdown(
-                  chapterData[core.ws.overview.explanationId].description,
-                  { useProse: true }
-                )
-              ) : (
-                <p className="mt-20 mb-20">
-                  Löse bitte mindestens eine Aufgabe aus dem vorherigen Kapitel,
-                  um dieses Kapitel zu starten.
-                </p>
+              {processMarkdown(
+                chapterData[core.ws.overview.explanationId].description,
+                { useProse: true }
               )}
             </div>
 
             <div className="flex justify-center mb-8 font-semibold">
-              {canContinue ? (
-                <button
-                  className="px-4 py-2 bg-green-200 hover:bg-green-300 rounded-lg"
-                  onClick={() => {
-                    setQuestData({
-                      id: core.ws.overview.explanationId,
-                      completed: true,
-                      code: '',
-                      mode: 'blocks',
-                      completedOnce: true,
-                    })
-                    submitAnalyzeEvent(
-                      core,
-                      'ev_click_explanation_continue_' +
-                        core.ws.overview.explanationId
-                    )
-                    closeModal(core)
-                  }}
-                >
-                  Weiter
-                </button>
-              ) : (
-                <button
-                  className="px-4 py-2 hover:bg-gray-300 rounded-lg"
-                  onClick={() => {
-                    closeModal(core)
-                  }}
-                >
-                  Schließen
-                </button>
-              )}
+              <button
+                className="px-4 py-2 bg-green-200 hover:bg-green-300 rounded-lg"
+                onClick={() => {
+                  setQuestData({
+                    id: core.ws.overview.explanationId,
+                    completed: true,
+                    code: '',
+                    mode: 'blocks',
+                    completedOnce: true,
+                  })
+                  submitAnalyzeEvent(
+                    core,
+                    'ev_click_explanation_continue_' +
+                      core.ws.overview.explanationId
+                  )
+                  closeModal(core)
+                }}
+              >
+                Weiter
+              </button>
             </div>
           </div>
         </div>
