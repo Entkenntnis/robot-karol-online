a3aac49ec0d2aa200d03ad838550b54bcf6b77f4
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-05-29 18:32:56 +0200

    add event for dance scores

--- lib/commands/messages.ts
+++ lib/commands/messages.ts
@@ -1,4 +1,6 @@
+import { pythonKarolExamples } from '../data/pythonExamples'
 import { Core } from '../state/core'
+import { submitAnalyzeEvent } from './analyze'
 
 export function addMessage(core: Core, text: string) {
   const ts = Date.now()
@@ -22,6 +24,22 @@ export function addMessage(core: Core, text: string) {
 
 export function addConsoleMessage(core: Core, text: string) {
   const ts = Date.now()
+
+  // =========================================
+  // special handler for dance, dance
+  const sharedId =
+    pythonKarolExamples.find((el) => el.title == 'Dance, Dance')?.link || ''
+  if (
+    '#' + core.ws.ui.sharedQuestId == sharedId &&
+    text.trim().endsWith(' Punkte')
+  ) {
+    const score = parseInt(text.split(' ')[0])
+    const title =
+      core.ws.quest.tasks[core.ws.quest.lastStartedTask!]?.title || ''
+    submitAnalyzeEvent(core, `ev_dance_score_${score}_${title}`)
+  }
+  // =========================================
+
   core.mutateWs(({ ui }) => {
     /*while (ui.messages.length >= 5) {
       ui.messages.shift()
