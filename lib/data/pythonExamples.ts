45e0cb6b364348d6eddc58c24a65abaf014bfe2b
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-06-11 21:52:35 +0200

    spend a little bit too much time on this ...

--- lib/data/pythonExamples.ts
+++ lib/data/pythonExamples.ts
@@ -17,7 +17,7 @@ export const pythonKarolExamples = [
   { title: 'Himmel und Hölle', link: '#5THY' },
   { title: 'Programmier-Witze', link: '#4NR3' },
   { title: 'Karol Kart', link: '#CXTJ' },
-  { title: 'Dance, Dance', link: '#B82N', hidden: true },
+  { title: 'Dance, Dance', link: '#CT2Z', hidden: true },
 ]
 
 export function getExampleId(title: string) {
