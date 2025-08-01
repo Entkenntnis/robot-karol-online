45f38e8d4f2f854f8b71e3cc56064a8ad7d63d28
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-08-01 22:34:37 +0200

    first rough draft of chapter 5

--- lib/data/chapters.ts
+++ lib/data/chapters.ts
@@ -1665,7 +1665,7 @@ const chapterInfo = [
     "title": "5. Streng geheim",
     "originalTitle": "Streng geheim",
     "requiredCount": 1,
-    "description": "# Streng geheim\r\n",
+    "description": "# Streng geheim\r\n\r\n_Ellie schreibt in ihr Tagebuch._\r\n\r\nIch mache gute Fortschritte beim Programmieren, es ist viel Arbeit, aber es macht Spaß, wenn man es verstanden hat. Manchmal träume ich auch in der Nacht von Python.\r\n\r\nWeißt du, was praktisch wäre? Ein Passwort für das Tagebuch. Ich könnte sowas jetzt programmieren. Dazu gibt es dieses `if/else`. Komme, ich zeige dir, wie das aussieht:\r\n\r\n```py\r\nprint(\"Streng geheim! Passwort:\")\r\npasswort = input()\r\n\r\nif passwort == \"Riley\":\r\n    print(\"Zugang erlaubt\")\r\nelse:\r\n    print(\"Geh weg!\")\r\n```\r\n\r\nAm Anfang war das verwirrend, aber mittlerweile habe ich es besser verstanden. Hinter dem `if` kann ich etwas überprüfen, zum Beispiel ob das Passwort korrekt ist. Ich muss zwei Gleichheitszeichen verwenden, weil es sonst mit den Variablen Verwirrung gibt. Wenn die Prüfung passt, wird der Code direkt danach ausgeführt (immer schön mit Tab einrücken, sonst geht alles kaputt).\r\n\r\nWenn die Prüfung nicht klappt, dann wird der Code im `else`-Block ausgeführt.\r\n\r\nIm Buch steht, das ich dadurch sehr intelligente Programme schreiben kann. Na, das werden wir noch sehen. So intelligent sieht das für mich noch nicht aus, aber ich wurde ja schon öfters überrascht.\r\n\r\nRiley, ich vermisse dich.\r\n",
     "image": "/story/journal.jpg",
     "quests": [],
     "x": 770,
--- lib/data/chapters/050-schleifen/info.md
+++ lib/data/chapters/050-schleifen/info.md
@@ -1,1 +1,25 @@
 # Streng geheim
+
+_Ellie schreibt in ihr Tagebuch._
+
+Ich mache gute Fortschritte beim Programmieren, es ist viel Arbeit, aber es macht Spaß, wenn man es verstanden hat. Manchmal träume ich auch in der Nacht von Python.
+
+Weißt du, was praktisch wäre? Ein Passwort für das Tagebuch. Ich könnte sowas jetzt programmieren. Dazu gibt es dieses `if/else`. Komme, ich zeige dir, wie das aussieht:
+
+```py
+print("Streng geheim! Passwort:")
+passwort = input()
+
+if passwort == "Riley":
+    print("Zugang erlaubt")
+else:
+    print("Geh weg!")
+```
+
+Am Anfang war das verwirrend, aber mittlerweile habe ich es besser verstanden. Hinter dem `if` kann ich etwas überprüfen, zum Beispiel ob das Passwort korrekt ist. Ich muss zwei Gleichheitszeichen verwenden, weil es sonst mit den Variablen Verwirrung gibt. Wenn die Prüfung passt, wird der Code direkt danach ausgeführt (immer schön mit Tab einrücken, sonst geht alles kaputt).
+
+Wenn die Prüfung nicht klappt, dann wird der Code im `else`-Block ausgeführt.
+
+Im Buch steht, das ich dadurch sehr intelligente Programme schreiben kann. Na, das werden wir noch sehen. So intelligent sieht das für mich noch nicht aus, aber ich wurde ja schon öfters überrascht.
+
+Riley, ich vermisse dich.
