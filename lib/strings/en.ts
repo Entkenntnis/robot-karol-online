124d4e7dd5c840834d83bcc550e73f23f9aef05b
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-07-30 22:50:33 +0200

    update version string

--- lib/strings/en.ts
+++ lib/strings/en.ts
@@ -9,7 +9,7 @@ export const enStrings: typeof deStrings = {
     load: 'Import progress',
     loadTooltip: 'Load from a file',
     gameState: 'gamestate',
-    version: 'Version: September 2025',
+    version: 'Version: August 2026',
     playground: 'Playground',
     imprint: 'Imprint',
     privacy: 'Privacy Policy',
