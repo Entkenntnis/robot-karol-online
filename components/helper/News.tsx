8a080cbf66176e2bcc385d6cb27c5126f54d6443
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-08-16 23:56:30 +0200

    prepare news

--- components/helper/News.tsx
+++ components/helper/News.tsx
@@ -9,6 +9,11 @@ export function News() {
     <div className="w-[760px] md:mx-auto border-emerald-600 border rounded mx-3 mb-32 px-4 pt-2 [&_a]:text-blue-500 hover:[&_a]:underline">
       <h2 className="text-2xl">Neuigkeiten</h2>
       <p className="my-4">
+        <b>XX. August 2026</b>: Die Spielwiese wurde überarbeitet und verhält
+        sich jetzt viel ähnlicher zum ursprünglichen Robot Karol: Die Welt
+        bleibt erhalten und Karol kann manuell gesteuert werden.
+      </p>
+      <p className="my-4">
         <b>4. August 2026</b>: Das Layout der Startseite wurde überarbeitet und
         bietet direkteren Zugriff auf wichtige Funktionen, wie die Auflistung
         aller Aufgaben oder dem Speichern des Fortschritts. Außerdem wurde
